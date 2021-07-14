c10515b11919e6a7dde3957d60302a70a75afd33
Brandon Tam <brandon.tam29@gmail.com>
2021-07-13 21:33:05 -0400

    tutorial 1 finished

---
+++ Assignment1/Question1/src/PublicTransportation.java
@@ -0,0 +1,12 @@
+public class PublicTransportation {
+	// Data Members
+	private double ticketPrice;
+	private int numOfStops;
+
+	// Constructor
+	public PublicTransportation(double ticketPrice, int numOfStops) {
+
+		this.ticketPrice = ticketPrice;
+		this.numOfStops = numOfStops;
+	}
+}

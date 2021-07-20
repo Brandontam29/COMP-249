7bd1c00b6d93f70d1c243ba800ff8b4032083aa6
Brandon Tam <brandon.tam29@gmail.com>
2021-07-20 12:08:04 -0400

    started assignment 2

---
+++ Assignment2/Problem1/src/Truck.java
@@ -0,0 +1,15 @@
+
+public class Truck {
+private String driverName;
+private String originatingCity;
+private String destinationCity;
+
+private int unloadedWeight;
+private int drivenWeight;
+private int loadedWeight;
+
+private int packageCount;
+private Package[] packages;
+
+private int grossIncome;
+}
